f3697913e01a6485dc8663ff904e0272c92a7471
nnsjiang <nnsjiang@tencent.com>
2024-04-08 17:08:19 +0800

    修改readme

--- arplugin-uniapp-demo/src/utils/auth.js
+++ arplugin-uniapp-demo/src/utils/auth.js
@@ -1,31 +1,31 @@
-const LICENSE_KEY = '48bd7bd4c8e221eb150fd1bf7d5a0ea4';
-const APP_ID = '1309122168';
+// 鉴权参考：https://cloud.tencent.com/document/product/616/71364
 
-function getSignature() {
-  return new Promise((resolve, reject) => {
-    wx.request({
-      url: 'https://service-8wkf8fy8-1258344699.gz.apigw.tencentcs.com/release/get-ar-sign',
-      method: 'GET',
-      success(res) {
-        console.log('getSignature ok', res);
-        resolve(res.data);
-      },
-      fail(e) {
-        console.log('getSignature error', e);
-      },
-    });
-  });
-}
+const LICENSE_KEY = '<your license key>';
+const APP_ID = '<your app id>';
+// token放在前端有泄漏风险，建议按照文档说明，将签名服务放到后端
+const TOKEN = '<your token>';
+
+import { SHA256 } from 'crypto-js';
+
+const sha256 = (str) => {
+  return SHA256(str).toString();
+};
+
+const genSignature = function () {
+  const timestamp = Math.round(new Date().getTime() / 1000);
+  const signature = sha256(timestamp + TOKEN + APP_ID + timestamp).toUpperCase(); // 使用上面获取到的token和appid合成加密串返回
+  return { signature, timestamp };
+};
 
 async function authFunc() {
-  const { signature, timestamp } = await getSignature();
+  const { signature, timestamp } = genSignature();
   return {
     signature,
     timestamp,
   };
 }
 
-module.exports = {
+export default {
   LICENSE_KEY,
   APP_ID,
   authFunc,
--- arplugin-uniapp-tuikit/src/TUICallKit/debug/webar-auth.js
+++ arplugin-uniapp-tuikit/src/TUICallKit/debug/webar-auth.js
@@ -1,19 +1,34 @@
-import crypto from "crypto-js";
-const LICENSE_KEY = "48bd7bd4c8e221eb150fd1bf7d5a0ea4";
-const APP_ID = "1309122168";
-const TOKEN = "1054493363136cf02c1ad7c54e3a9c76";
+// 鉴权参考：https://cloud.tencent.com/document/product/616/71364
+
+const LICENSE_KEY = "<your license key>";
+const APP_ID = "<your app id>";
+// token放在前端有泄漏风险，建议按照文档说明，将签名服务放到后端
+const TOKEN = "<your token>";
+
+import { SHA256 } from "crypto-js";
 
 const sha256 = (str) => {
-  return crypto.SHA256(str).toString();
+  return SHA256(str).toString();
 };
 
-// 此方法仅测试时使用，为防止 token 泄露，发布时请使用服务端加密，详见[腾讯云-腾讯特效 SDK 官网文档](https://cloud.tencent.com/document/product/616/71364)
-function authFunc() {
+const genSignature = function () {
   const timestamp = Math.round(new Date().getTime() / 1000);
   const signature = sha256(
     timestamp + TOKEN + APP_ID + timestamp
-  ).toUpperCase();
+  ).toUpperCase(); // 使用上面获取到的token和appid合成加密串返回
   return { signature, timestamp };
+};
+
+async function authFunc() {
+  const { signature, timestamp } = genSignature();
+  return {
+    signature,
+    timestamp,
+  };
 }
 
-export { LICENSE_KEY, APP_ID, authFunc };
+export default {
+  LICENSE_KEY,
+  APP_ID,
+  authFunc,
+};
